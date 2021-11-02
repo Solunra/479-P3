8b3a3f2a8639506fd11f7d61c5e7490aeed8d8f8
Solunra <sunnydeng90@gmail.com>
2021-11-02 16:10:04 -0400

    Initial conversion to NEWID

---
+++ main.py
@@ -0,0 +1,145 @@
+import test_resources
+import nltk
+import re
+
+
+def main():
+    nltk.download('punkt', quiet=True)
+    allDocuments = []
+    for index in range(0, 22):
+        allDocuments.append(test_resources.main(f'{str(index).zfill(3)}'))
+    vocabulary = naive_indexer(allDocuments)
+    terms = ['pineapple', 'Phillippines', 'Brierley', 'Chrysler']
+    for term in terms:
+        result = single_term_query_processing(term, vocabulary)
+        with open(f'postings-of-{term}.txt', 'w+') as file:
+            file.write(', '.join([str(docId) for docId in result]))
+    lossy_compression_table(vocabulary, get_stop_words_25(), get_stop_words_126())
+
+
+# Task 1; Accepts 'Documents' as a docId -> list of tokens
+def naive_indexer(documents):
+    # save as word -> [listOf]
+    vocabularyDict = {}
+    # add to dictionary
+    for docId, document in documents.items():
+        for token in document:
+            if token not in vocabularyDict.keys():
+                vocabularyDict[token] = [docId]
+            else:
+                if docId not in vocabularyDict[token]:
+                    vocabularyDict[token].append(docId)
+
+    # sort dictionary values
+    for key, value in vocabularyDict.items():
+        value.sort()
+        vocabularyDict[key] = value
+    return vocabularyDict
+
+
+# Task 2; Assume query is a singular term, vocabulary as the same as Task 1's return
+def single_term_query_processing(query, vocabulary):
+    doc_ids = vocabulary.get(query)
+    if doc_ids is None:
+        return []
+    else:
+        return doc_ids
+
+
+# Task 3; Lossy Compression; documents is a list of tokens
+def lossy_compression_table(vocabulary, stop_words_small, stop_words_big):
+    # lower cases everything
+    no_numbers_tokens = {token: vocabulary[token] for token in vocabulary if not re.search('\d', token)}
+    case_folded_tokens = {}
+    for token in no_numbers_tokens:
+        lower_case_token = token.lower()
+        if lower_case_token not in case_folded_tokens.keys():
+            case_folded_tokens[lower_case_token] = no_numbers_tokens[token]
+        else:
+            current_tokens = case_folded_tokens[lower_case_token]
+            other_tokens = no_numbers_tokens[token]
+            if current_tokens is not None and other_tokens is not None:
+                case_folded_tokens[lower_case_token] = list(set(current_tokens).union(set(other_tokens)))
+    stop_word_filtered_tokens_25 = {token: case_folded_tokens[token] for token in case_folded_tokens if token not in stop_words_small}
+    stop_word_filtered_tokens_126 = {token: stop_word_filtered_tokens_25[token] for token in stop_word_filtered_tokens_25 if token not in stop_words_big}
+    ps = nltk.stem.PorterStemmer()
+    stemmed_tokens = {}
+    for token in stop_word_filtered_tokens_126:
+        stemmed_token = ps.stem(token)
+        if stemmed_token not in stemmed_tokens.keys():
+            stemmed_tokens[stemmed_token] = stop_word_filtered_tokens_126[token]
+        else:
+            current_tokens = stemmed_tokens[stemmed_token]
+            other_tokens = stop_word_filtered_tokens_126[token]
+            if current_tokens is not None and other_tokens is not None:
+                stemmed_tokens[stemmed_token] = list(set(current_tokens).union(set(other_tokens)))
+    # counting for postings size
+    unfiltered_postings_size = 0
+    for postings in vocabulary:
+        unfiltered_postings_size = unfiltered_postings_size + len(postings)
+    no_numbers_tokens_postings_size = 0
+    for postings in no_numbers_tokens:
+        no_numbers_tokens_postings_size = no_numbers_tokens_postings_size + len(postings)
+    case_folded_tokens_postings_size = 0
+    for postings in case_folded_tokens:
+        case_folded_tokens_postings_size = case_folded_tokens_postings_size + len(postings)
+    stop_word_filtered_tokens_postings_size_25 = 0
+    for postings in stop_word_filtered_tokens_25:
+        stop_word_filtered_tokens_postings_size_25 = stop_word_filtered_tokens_postings_size_25 + len(postings)
+    stop_word_filtered_tokens_postings_size_126 = 0
+    for postings in stop_word_filtered_tokens_126:
+        stop_word_filtered_tokens_postings_size_126 = stop_word_filtered_tokens_postings_size_126 + len(postings)
+    stemmed_tokens_postings_size = 0
+    for postings in stemmed_tokens:
+        stemmed_tokens_postings_size = stemmed_tokens_postings_size + len(postings)
+
+    with open(f'./table-results.txt', 'w+') as file:
+        file.write(f'               |      distinct terms       |\n')
+        file.write(f'               |===========================|\n')
+        file.write(f'               | number | delta % | term % |\n')
+        file.write(f'               |===========================|\n')
+        file.write(f'unfiltered     |{str(len(vocabulary)):8}|    -    |   -    |\n')
+        file.write(f'no numbers     |{str(len(no_numbers_tokens)):8}|{(100 * (len(no_numbers_tokens) - len(vocabulary)) / len(vocabulary)):3.6f}|{(100 * (len(no_numbers_tokens) - len(vocabulary)) / len(vocabulary)):3.5f}|\n')
+        file.write(f'case folding   |{str(len(case_folded_tokens)):8}|{(100*(len(case_folded_tokens)-len(no_numbers_tokens))/len(no_numbers_tokens)):3.6f}|{(100 * (len(case_folded_tokens) - len(vocabulary)) / len(vocabulary)):3.5f}|\n')
+        file.write(f'25 stop words  |{str(len(stop_word_filtered_tokens_25)):8}|{(100*(len(stop_word_filtered_tokens_25)-len(case_folded_tokens))/len(case_folded_tokens)):3.6f}|{(100 * (len(stop_word_filtered_tokens_25) - len(vocabulary)) / len(vocabulary)):3.5f}|\n')
+        file.write(f'126 stop words |{str(len(stop_word_filtered_tokens_126)):8}|{(100*(len(stop_word_filtered_tokens_126)-len(stop_word_filtered_tokens_25))/len(stop_word_filtered_tokens_25)):3.6f}|{(100 * (len(stop_word_filtered_tokens_126) - len(vocabulary)) / len(vocabulary)):3.5f}|\n')
+        file.write(f'stemming       |{str(len(stemmed_tokens)):8}|{(100*(len(stemmed_tokens)-len(stop_word_filtered_tokens_126))/len(stop_word_filtered_tokens_126)):3.6f}|{(100 * (len(stemmed_tokens) - len(vocabulary)) / len(vocabulary)):3.5f}|\n')
+        file.write(f'\n\n')
+        file.write(f'               |             postings list size              |\n')
+        file.write(f'               |=============================================|\n')
+        file.write(f'               |    number    |    delta %    |    size %    |\n')
+        file.write(f'               |=============================================|\n')
+        file.write(f'unfiltered     |{str(unfiltered_postings_size):14}|       -       |      -       |\n')
+        file.write(f'no numbers     |{str(no_numbers_tokens_postings_size):14}|{(100*(no_numbers_tokens_postings_size-unfiltered_postings_size)/unfiltered_postings_size):3.12f}|{(100*(no_numbers_tokens_postings_size-unfiltered_postings_size)/unfiltered_postings_size):3.11f}|\n')
+        file.write(f'case folding   |{str(case_folded_tokens_postings_size):14}|{(100*(case_folded_tokens_postings_size-no_numbers_tokens_postings_size)/no_numbers_tokens_postings_size):3.12f}|{(100*(case_folded_tokens_postings_size-unfiltered_postings_size)/unfiltered_postings_size):3.11f}|\n')
+        file.write(f'25 stop words  |{str(stop_word_filtered_tokens_postings_size_25):14}|{(100*(stop_word_filtered_tokens_postings_size_25-case_folded_tokens_postings_size)/case_folded_tokens_postings_size):3.12f}|{(100*(stop_word_filtered_tokens_postings_size_25-unfiltered_postings_size)/unfiltered_postings_size):3.11f}|\n')
+        file.write(f'126 stop words |{str(stop_word_filtered_tokens_postings_size_126):14}|{(100*(stop_word_filtered_tokens_postings_size_126-stop_word_filtered_tokens_postings_size_25)/stop_word_filtered_tokens_postings_size_25):3.12f}|{(100*(stop_word_filtered_tokens_postings_size_126-unfiltered_postings_size)/unfiltered_postings_size):3.11f}|\n')
+        file.write(f'stemming       |{str(stemmed_tokens_postings_size):14}|{(100*(stemmed_tokens_postings_size-stop_word_filtered_tokens_postings_size_126)/stop_word_filtered_tokens_postings_size_126):3.12f}|{(100*(stemmed_tokens_postings_size-unfiltered_postings_size)/unfiltered_postings_size):3.11f}|\n')
+
+
+def get_stop_words_126():
+    return ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are',
+            'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but',
+            'by', 'cannot', 'could', 'did', 'do', 'does', 'doing', 'down', 'during', 'each', 'few',
+            'for', 'from', 'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here', 'hers',
+            'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in', 'into', 'is', 'it', 'its',
+            'itself', 'me', 'more', 'most', 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on',
+            'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own',
+            'same', 'she', 'should', 'so', 'some', 'such', 'than', 'that', 'the', 'their', 'theirs',
+            'them', 'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 'through', 'to',
+            'too', 'under', 'until', 'up', 'very', 'was', 'we', 'were', 'what', 'when', 'where',
+            'which', 'while', 'who', 'whom', 'why', 'with', 'will', 'would', 'you', 'your', 'yours', 'yourself',
+            'yourselves']
+
+
+# From https://nlp.stanford.edu/IR-book/html/htmledition/dropping-common-terms-stop-words-1.html#sec:stopwords
+def get_stop_words_25():
+    return ['a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he', 'in', 'it', 'its', 'of', 'on',
+            'that', 'the', 'to', 'was', 'were', 'will', 'with']
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+
---
+++ test_resources.py
@@ -0,0 +1,57 @@
+import re
+import nltk
+
+
+# All code taken from P1
+def main(file_number):
+    # quiet so it doesn't output to terminal about it being downloaded already
+    nltk.download('punkt', quiet=True)
+    return run_all_in_pipeline(f'./reuters/reut2-{file_number}.sgm')
+
+
+def run_all_in_pipeline(file_location):
+    dict_of_texts = read_and_extract_raw_text(file_location)
+    dict_of_tokens = tokenize(dict_of_texts)
+    return dict_of_tokens
+
+
+def read_and_extract_raw_text(file_location):
+    unallowed_symbols = ['&', '<', '>']
+    all_text = ""
+    all_body_and_title = ""
+    with open(file_location) as file:
+        all_text += file.read()
+    split_text = all_text.split('</REUTERS>')
+    # returned dict of id -> list of tokens
+    dict_of_texts = {}
+    for text in split_text:
+        if text:
+            all_body_and_title = ""
+            # Using regex to find the newID of the reuters collection
+            doc_id = re.findall(f'NEWID="(.*)">')
+            # Using REGEX to find all files between the specified tags
+            # Both TITLE and BODY only occurs at specific points
+            title = re.findall(f"<TITLE>(.*)</TITLE>", text, flags=re.DOTALL)
+            body = re.findall(f"<BODY>(.*)</BODY>", text, flags=re.DOTALL)
+            if title:
+                all_body_and_title += title[0] + ' '
+            if body:
+                all_body_and_title += body[0] + ' '
+            dict_of_texts[doc_id] = all_body_and_title
+    return dict_of_texts
+
+
+def string_contains_list(str_to_check, unallowed_chars):
+    for element in unallowed_chars:
+        result = str_to_check.find(element)
+        if result != -1:
+            return True
+    return False
+
+
+def tokenize(dict_of_texts):
+    dict_of_tokens = {}
+    for docID, text in dict_of_texts.items():
+        tokens = nltk.word_tokenize(text)
+        dict_of_tokens[docID] = tokens
+    return dict_of_tokens
